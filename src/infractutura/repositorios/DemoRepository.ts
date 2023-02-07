e5f6d0c524c76596a89c62ef2ad96b116408b74e
angieAGL <angelinajavierarica@gmail.com>
2023-02-07 13:54:12 -0300

    Capa de aplicacion terminada

--- src/infractutura/repositorios/DemoRepository.ts
+++ src/infractutura/repositorios/DemoRepository.ts
@@ -1,13 +1,16 @@
 import { Demo } from "../../dominio/Demo";
 import DATASET_DEMO from "../dataSet/DATASET_DEMO.json";
+import { IDemoRepository } from "../../aplicacion/interfaces/IDemoRepository";
 
-export const listarDemo = () => {
-  let listado_demo: Demo[] = [];
-  let listado_json = DATASET_DEMO;
+export class DemoRepository implements IDemoRepository {
+  listarDemo(): Demo[] {
+    let listado_demo: Demo[] = [];
+    let listado_json = DATASET_DEMO;
 
-  for (var i in listado_json) {
-    listado_demo.push(listado_json[i]);
+    for (var i in listado_json) {
+      listado_demo.push(listado_json[i]);
+    }
+
+    return listado_demo;
   }
-
-  return listado_demo;
-};
+}
