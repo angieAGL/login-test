24e1de513abfe7fdce7c42a735218916a99b37a3
angieAGL <angelinajavierarica@gmail.com>
2023-02-23 14:20:13 -0300

    Agregar metodo delete en DemoRepository

--- src/infraestructura/repositorios/DemoRepository.ts
+++ src/infraestructura/repositorios/DemoRepository.ts
@@ -4,13 +4,21 @@ import { IDemoRepository } from "../../aplicacion/interfaces/IDemoRepository";
 
 export class DemoRepository implements IDemoRepository {
   listarDemo(): Demo[] {
-    let listado_demo: Demo[] = [];
     let listado_json = DATASET_DEMO;
+    return listado_json;
+  }
 
-    for (var i in listado_json) {
-      listado_demo.push(listado_json[i]);
+  delete(id: number): boolean {
+    let listaDemo = this.listarDemo();
+    listaDemo[0].activo = true;
+
+    for (var i in listaDemo) {
+      if (listaDemo[i].id === id) {
+        listaDemo[i].activo = false;
+        return true;
+      }
     }
 
-    return listado_demo;
+    return false;
   }
 }
