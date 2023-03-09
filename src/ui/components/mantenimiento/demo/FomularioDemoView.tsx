bcf79d37fe0f0d3560b4847b320963c753c49073
angieAGL <angelinajavierarica@gmail.com>
2023-03-09 12:55:11 -0300

    Modificando estilo a view de agregar demo

--- src/ui/components/mantenimiento/demo/FomularioDemoView.tsx
+++ src/ui/components/mantenimiento/demo/FomularioDemoView.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Form } from "react-bootstrap";
+import { Col, Container, Form, Row, Stack } from "react-bootstrap";
 import { useLocation, useNavigate } from "react-router-dom";
 import { Demo } from "../../../../dominio/entidades/Demo";
 import { DemoEvents } from "../../../../presentacion/DemoEvents";
@@ -53,116 +53,161 @@ const FormularioDemoView = () => {
     });
   };
 
-  console.log(parametros);
-
   return (
-    <div>
+    <Container className="d-grid">
+      <h1 className="titulo">Agregar Demo </h1>
       <Form onSubmit={handleSubmit(funcionSubmit)}>
-        <Form.Group className="mb-2">
-          <Form.Label className="form-label ">ID</Form.Label>
-          <Form.Control
-            disabled={parametros.modoEditar}
-            className={
-              mensajeErrorId ? "form-control error" : "form-control correcto"
-            }
-            type="number"
-            {...idValidar}
-          ></Form.Control>
-          {<p style={{ color: "var(--color-error)" }}>{mensajeErrorId}</p>}
-        </Form.Group>
-        <Form.Group className="mb-2">
-          <Form.Label className="form-label">Nombre</Form.Label>
-          <Form.Control
-            className={
-              mensajeErrorNombre
-                ? "form-control error"
-                : "form-control correcto"
-            }
-            type="text"
-            {...nombreValidar}
-          ></Form.Control>
-          {<p style={{ color: "var(--color-error)" }}>{mensajeErrorNombre}</p>}
-        </Form.Group>
-        <Form.Group className="mb-2">
-          <Form.Label className="form-label">Apellido</Form.Label>
-          <Form.Control
-            className={
-              mensajeErrorApellido
-                ? "form-control error"
-                : "form-control correcto"
-            }
-            type="text"
-            {...apellidoValidar}
-          ></Form.Control>
-          {
-            <p style={{ color: "var(--color-error)" }}>
-              {mensajeErrorApellido}
-            </p>
-          }
-        </Form.Group>
-        <Form.Group className="mb-2">
-          <Form.Label className="form-label">Email</Form.Label>
-          <Form.Control
-            className={
-              mensajeErrorEmail ? "form-control error" : "form-control correcto"
-            }
-            type="email"
-            {...emailValidar}
-          ></Form.Control>
-          {<p style={{ color: "var(--color-error)" }}>{mensajeErrorEmail}</p>}
-        </Form.Group>
-        <Form.Group className="mb-2">
-          <Form.Label className="form-label">Genero</Form.Label>
-          <Form.Select
-            className={
-              mensajeErrorGenero
-                ? "form-control error"
-                : "form-control correcto"
-            }
-            {...generoValidar}
-          >
-            <option value={0}>Seleccione</option>
-            {options};
-          </Form.Select>
-          {<p style={{ color: "var(--color-error)" }}>{mensajeErrorGenero}</p>}
-        </Form.Group>
-        <Form.Group className="mb-2">
-          <Form.Label className="form-label">Numero</Form.Label>
-          <Form.Control
-            className={
-              mensajeErrorNumero
-                ? "form-control error"
-                : "form-control correcto"
-            }
-            type="number"
-            {...numeroValidar}
-          ></Form.Control>
-          {<p style={{ color: "var(--color-error)" }}>{mensajeErrorNumero}</p>}
-        </Form.Group>
-        <Form.Group className="mb-2">
-          <Form.Label className="form-label">Fecha</Form.Label>
-          <Form.Control
-            className={
-              mensajeErrorFecha
-                ? "form-control-sm error"
-                : "form-control-sm correcto"
-            }
-            type="date"
-            id={"fecha"}
-            {...fechaValidar}
-          ></Form.Control>
-          {<p style={{ color: "var(--color-error)" }}>{mensajeErrorFecha}</p>}
-        </Form.Group>
-        <Boton
-          class_name={"btn-cerrar"}
-          href={"/demo"}
-          text={"Cancelar"}
-          type={"button"}
-        />
+        <Row className="mt-3">
+          <Col sm>
+            <Form.Group className="mb-2">
+              <Form.Label className="form-label ">ID</Form.Label>
+              <Form.Control
+                disabled={parametros.modoEditar}
+                className={
+                  mensajeErrorId
+                    ? "form-control error"
+                    : "form-control correcto"
+                }
+                type="number"
+                {...idValidar}
+              ></Form.Control>
+              {<p style={{ color: "var(--color-error)" }}>{mensajeErrorId}</p>}
+            </Form.Group>
+          </Col>
+          <Col sm>
+            <Form.Group className="mb-2">
+              <Form.Label className="form-label">Nombre</Form.Label>
+              <Form.Control
+                className={
+                  mensajeErrorNombre
+                    ? "form-control error"
+                    : "form-control correcto"
+                }
+                type="text"
+                {...nombreValidar}
+              ></Form.Control>
+              {
+                <p style={{ color: "var(--color-error)" }}>
+                  {mensajeErrorNombre}
+                </p>
+              }
+            </Form.Group>
+          </Col>
+        </Row>
+        <Row>
+          <Col sm>
+            <Form.Group className="mb-2">
+              <Form.Label className="form-label">Apellido</Form.Label>
+              <Form.Control
+                className={
+                  mensajeErrorApellido
+                    ? "form-control error"
+                    : "form-control correcto"
+                }
+                type="text"
+                {...apellidoValidar}
+              ></Form.Control>
+              {
+                <p style={{ color: "var(--color-error)" }}>
+                  {mensajeErrorApellido}
+                </p>
+              }
+            </Form.Group>
+          </Col>
+          <Col sm>
+            <Form.Group className="mb-2">
+              <Form.Label className="form-label">Email</Form.Label>
+              <Form.Control
+                className={
+                  mensajeErrorEmail
+                    ? "form-control error"
+                    : "form-control correcto"
+                }
+                type="email"
+                {...emailValidar}
+              ></Form.Control>
+              {
+                <p style={{ color: "var(--color-error)" }}>
+                  {mensajeErrorEmail}
+                </p>
+              }
+            </Form.Group>
+          </Col>
+        </Row>
+        <Row>
+          <Col sm>
+            <Form.Group className="mb-2">
+              <Form.Label className="form-label">Genero</Form.Label>
+              <Form.Select
+                className={
+                  mensajeErrorGenero
+                    ? "form-control error"
+                    : "form-control correcto"
+                }
+                {...generoValidar}
+              >
+                <option value={0}>Seleccione</option>
+                {options};
+              </Form.Select>
+              {
+                <p style={{ color: "var(--color-error)" }}>
+                  {mensajeErrorGenero}
+                </p>
+              }
+            </Form.Group>
+          </Col>
+          <Col sm>
+            <Form.Group className="mb-2">
+              <Form.Label className="form-label">Numero</Form.Label>
+              <Form.Control
+                className={
+                  mensajeErrorNumero
+                    ? "form-control error"
+                    : "form-control correcto"
+                }
+                type="number"
+                {...numeroValidar}
+              ></Form.Control>
+              {
+                <p style={{ color: "var(--color-error)" }}>
+                  {mensajeErrorNumero}
+                </p>
+              }
+            </Form.Group>
+          </Col>
+          <Col sm>
+            <Form.Group className="mb-2">
+              <Form.Label className="form-label">Fecha</Form.Label>
+              <Form.Control
+                className={
+                  mensajeErrorFecha
+                    ? "form-control error"
+                    : "form-control correcto"
+                }
+                type="date"
+                id={"fecha"}
+                {...fechaValidar}
+              ></Form.Control>
+              {
+                <p style={{ color: "var(--color-error)" }}>
+                  {mensajeErrorFecha}
+                </p>
+              }
+            </Form.Group>
+          </Col>
+        </Row>
+        <Stack direction="horizontal" gap={4} className={"mt-3"}>
+          <Boton
+            class_name={"btn-cerrar ms-auto"}
+            href={"/demo"}
+            text={"Cancelar"}
+            type={"button"}
+          />
 
-        <Boton text={"Guardar"} type={"submit"} />
+          <Boton text={"Guardar"} type={"submit"} />
+        </Stack>
       </Form>
-    </div>
+    </Container>
   );
 };
 
