84703af50cc4f8efe1779637fb802c9fd01175d7
angieAGL <angelinajavierarica@gmail.com>
2023-04-06 18:43:21 -0400

    Cambio de estilo a sidebar

--- src/ui/components/errores/NotFoundView.tsx
+++ src/ui/components/errores/NotFoundView.tsx
@@ -1,9 +1,8 @@
-import React from "react";
 import { Col, Container, Row } from "react-bootstrap";
-import Boton from "./common/grids/botones/Boton";
-import "../css/errorView.css";
+import Boton from "../common/grids/botones/Boton";
+import "../../css/notFound.css";
 
-const error = require("../assets/img/error404.png");
+const error = require("../../assets/img/error404.png");
 
 const ErrorView = () => {
   return (
