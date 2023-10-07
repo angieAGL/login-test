166033eb63fb2170b25fa7050374f2750cb71568
angieAGL <91481205+angieAGL@users.noreply.github.com>
2023-10-06 22:46:25 -0300

    Update DemoRepository.ts

--- src/infraestructura/repositorios/DemoRepository.ts
+++ src/infraestructura/repositorios/DemoRepository.ts
@@ -9,7 +9,7 @@ export class DemoRepository implements IDemoRepository {
  
     let data = await response.json();
     console.log(data);   
-    let respo1= await fetch("http://api-admin-load-balancer-dev-1144647766.us-east-1.elb.amazonaws.com/notificacion/leer_todo")
+    let respo1= await fetch("https://apibackoffice.proinvesting.co/notificacion/leer_todo")
     let data1= await response.json();
     console.log(data1);
     return data;
