45e75619232fd75b52f3a041808b261811786ba5
Angie <angelinajavierarica@gmail.com>
2023-11-03 10:38:16 -0300

    Cambios de test y para probar github actions

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,8 +1,24 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { fireEvent, render, screen } from "@testing-library/react";
+import React from "react";
+import FormatDate from "./ui/components/common/FormatoFecha"
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe("FormatDate", () => {
+  it("should return a div element with the formatted date when given a valid date string", () => {
+    const date = "2022-01-01";
+    render(FormatDate(date));
+    const formattedDate = screen.getByText("01-01-2022");
+    expect(formattedDate).toBeInTheDocument();
+  });
+
+  it("should return an empty div element when given an empty date string", () => {
+    const date = "";
+    const { container } = render(FormatDate(date));
+    expect(container.firstChild).toBeInTheDocument();
+  });
+
+  it("should return an empty div element when given an null date string", () => {
+    const date = "null";
+    const { container } = render(FormatDate(date));
+    expect(container.firstChild).toBeInTheDocument();
+  });
 });
